0a984ddecc34ce02e6548509c70999cd57599648
Miles Talmey <mtalmey@equalexperts.com>
2024-02-20 14:08:43 +0000

    copyright

--- src/main/java/uk/emarte/regurgitator/extensions/swagger/XmlUtil.java
+++ src/main/java/uk/emarte/regurgitator/extensions/swagger/XmlUtil.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2017 Miles Talmey.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
 package uk.emarte.regurgitator.extensions.swagger;
 
 import org.w3c.dom.Document;
